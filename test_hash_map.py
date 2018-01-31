d9d590f17dda8d9dbf1dfd230a690e631c5df63d
Lucas Lofaro <lucasmlofaro@gmail.com>
2018-01-30 21:08:15 -0800

    Add resize test

--- test_hash_map.py
+++ test_hash_map.py
@@ -7,6 +7,13 @@ class TestHashMap(unittest.TestCase):
         self.hash_map = HashMap()
         self.item = Item('key', 'value')
 
+    def test_resize(self):
+        self.hash_map.insert(self.item)
+        self.assertEqual(len(self.hash_map.buckets), 1)
+
+        self.hash_map.insert(Item('new_key', 'new_value'))
+        self.assertEqual(len(self.hash_map.buckets), 2)
+
     def test_insert(self):
         self.hash_map.insert(self.item)
 
