0389c0db4b451aa402873abc4122a14425426d1f
Lucas Lofaro <lucasmlofaro@gmail.com>
2018-01-30 21:07:46 -0800

    Move hash_map object to setUp

--- test_hash_map.py
+++ test_hash_map.py
@@ -4,15 +4,15 @@ from hash_map import Bucket, HashMap, Item
 
 class TestHashMap(unittest.TestCase):
     def setUp(self):
+        self.hash_map = HashMap()
         self.item = Item('key', 'value')
 
     def test_insert(self):
-        hash_map = HashMap()
-        hash_map.insert(self.item)
+        self.hash_map.insert(self.item)
 
-        self.assertEqual(hash_map._count, 1)
-        self.assertEqual(len(hash_map.buckets), 1)
-        self.assertEqual(hash_map.buckets[0].next(), self.item)
+        self.assertEqual(self.hash_map._count, 1)
+        self.assertEqual(len(self.hash_map.buckets), 1)
+        self.assertEqual(self.hash_map.buckets[0].next(), self.item)
 
 
 if __name__ == '__main__':
